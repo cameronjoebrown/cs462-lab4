7b010df96a8f509ab572c6b918cbc904e44ad097
Cameron Brown <cameronjoebrown@gmail.com>
2022-02-08 21:22:26 -0700

    Setup temperature

--- temperatures/src/app/app.component.ts
+++ temperatures/src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'temperatures';
 
-  getTemperatures() {
+  temperatures: any;
 
-  }
+  constructor(private api: ApiService) {}
 
-  getViolations() {
-    
+  ngOnInit(): void {
+      this.api.getTemperatures().subscribe((response: any) => {
+        this.temperatures = response;
+        console.log(this.temperatures);
+      })
   }
 }
