5088de2e19a52ad55908701221b349eeece1d9d0
Cameron Brown <cameronjoebrown@gmail.com>
2022-02-08 21:21:57 -0700

    Create api service

---
+++ temperatures/src/app/services/api.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  temperatures: any;
+
+  cloudUrl = "http://localhost:3000/sky/cloud/cky9r3iga000vw7zva2fb7v1z";
+  headerOptions: any;
+
+  constructor(private http: HttpClient) {
+    this.headerOptions = {
+      headers: new HttpHeaders({
+        "Content-type": "application/json",
+      }),
+      body: {}
+    }
+  }
+
+  getTemperatures() {
+    return this.http.get(this.cloudUrl + '/temperature_store/temperatures', this.headerOptions);
+  }
+
+  getViolations() {
+
+  }
+}
